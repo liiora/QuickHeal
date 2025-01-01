e1fe6d894d7cedf6575183b4874af5ca7b1c0d0f
Bestoriop <134808420+Bestoriop@users.noreply.github.com>
2025-01-01 18:19:02 +0100

    Holy judgement fix

--- QuickHealPaladin.lua
+++ QuickHealPaladin.lua
@@ -113,7 +113,7 @@ function QuickHeal_Paladin_FindSpellToUse(Target, healType, multiplier, forceMax
     -- Detect proc of 'Holy Judgement" (next Holy Light is fast cast)
     if QuickHeal_DetectBuff('player',"ability_paladin_judgementblue") then
         debug("BUFF: Holy Judgement (out of combat healing forced)");
-        InCombat = false;
+        forceMaxHPS = false;
     end
 
     -- Get total healing modifier (factor) caused by healing target debuffs
